f2dbf4d19d10a93930e9757ab67ad4423d045008
achoo-o <amyj.etana@gmail.com>
2024-09-24 15:51:26 +1200

    add image, image styles

--- client/components/Projects.tsx
+++ client/components/Projects.tsx
@@ -1,22 +1,54 @@
 export default function Projects() {
+  const projects = [
+    {
+      name: 'pawpals', 
+      description: 'Made as part of the DevAcademy course in a group of 5, pawpals is a website made to bring pet-lovers together.', 
+      link: 'https://pawpals-matai-24.devacademy.nz/',
+      thumbnail: 'pawpals.png',
+      alt: ''
+    },
+    {
+      name: 'Whakataukī API', 
+      description: 'This project was made to promote Te Reo Māori usage in the developer space via Whakataukī, Māori "proverbs".', 
+      link: 'https://whakatauki-api.up.railway.app/',
+      thumbnail: 'whakatauki.png',
+      alt: ''
+    },
+    {
+      name: 'My \'First\' Project', 
+      description: 'A simple rock, paper, scissors project made as part of The Odin Project.', 
+      link: 'https://achoo-o.github.io/paper-scissors-rock/paperScissorsRock.html',
+      thumbnail: 'rockpaperscissors.png',
+      alt: ''
+      },
+  ]
   //https://flowbite.com/docs/components/card/
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-        <a href="#">
-            <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-        </a>
-        <div className="p-5">
-            <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-black">Noteworthy technology acquisitions 2021</h5>
+      {projects.map((project) => {
+        const {name, description, link, thumbnail, alt} = project
+        return(
+          <section key={name}>
+            <a href={link}>
+                <img className="rounded-t-lg  object-cover max-h-56 w-full" src={`images/${thumbnail}`} alt={alt} />
             </a>
-            <p className="mb-3 font-normal text-gray-800">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-400 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                View Project
-                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a>
-        </div>
+            <div className="p-5">
+                <a href={link}>
+                    <h1 className="mb-2 text-2xl font-bold tracking-tight text-black">{name}</h1>
+                </a>
+                <p className="mb-3 font-normal text-gray-800">
+                  {description}
+                </p>
+                <a href={link} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-400 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    View Project
+                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </a>
+            </div>
+        </section>
+        )
+      })}
     </div>
   )
 }
