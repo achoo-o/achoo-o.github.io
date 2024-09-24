4bca2a85ea4c3f21ba7c108ec0093c5e655337e2
achoo-o <amyj.etana@gmail.com>
2024-09-24 15:07:46 +1200

    add footer content

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
-    <></>
+    <section>
+      <ul>
+        <li>
+          <Link to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/projects">
+            Projects
+          </Link>
+        </li>
+      </ul>
+    </section>
   )
 }
