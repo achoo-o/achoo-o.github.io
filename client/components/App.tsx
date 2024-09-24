6579b497ea12987afff55ea7eb81f218fc4ba702
achoo-o <amyj.etana@gmail.com>
2024-09-24 15:05:39 +1200

    add footer, setup app -> oulet

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,5 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+
+
 export default function App() {
   return (
-    <></>
+    <>
+      <Outlet />
+      <Footer />
+    </>
   )
 }
--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <></>
+    <section>
+      <h1>Amy Jackson</h1>
+      <h2>Full Stack Web Developer</h2>
+    </section>
   )
 }
