c297074c7a88c9ddb8827fac3ba3de4cd9d432b6
achoo-o <amyj.etana@gmail.com>
2024-09-24 16:01:48 +1200

    refactor nav list

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,24 +1,22 @@
 import { Link } from "react-router-dom";
 
 export default function Footer() {
+  const nav = [
+    {name: 'Home', link: '/'},
+    {name: 'About', link: '/about'},
+    {name: 'Projects', link: '/projects'},
+  ]
   return (
     <section>
       <ul>
-        <li>
-          <Link to="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/about">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/projects">
-            Projects
-          </Link>
-        </li>
+        {nav.map((item, i) => {
+          return (
+          <li key={i}>
+            <Link to={item.link}>
+              {item.name}
+            </Link>
+          </li>
+        )})}
       </ul>
     </section>
   )
