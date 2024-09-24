375c4c107346dbc0d093a9816a16baaa8f7495a5
achoo-o <amyj.etana@gmail.com>
2024-09-24 15:00:02 +1200

    Add basic component files

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,5 @@
-function App() {
+export default function App() {
   return (
-    <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
-    </div>
+    <></>
   )
 }
-
-export default App
