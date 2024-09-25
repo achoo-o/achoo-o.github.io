0b0399615de8f0b19e38c94b39cff8afc06c1fb5
Amy <58102593+achoo-o@users.noreply.github.com>
2024-09-26 10:05:49 +1200

    remove devtools

--- client/index.tsx
+++ client/index.tsx
@@ -1,8 +1,7 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRoot } from 'react-dom/client'
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
 
 const queryClient = new QueryClient()
 const root = createRoot(document.getElementById('app') as HTMLElement)
@@ -11,7 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
   root.render(
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    </QueryClientProvider>,
   )
 })
