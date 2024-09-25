880776dc0e9f3f6fc5f6ac341e009ac787ea3585
achoo-o <amyj.etana@gmail.com>
2024-09-25 20:43:46 +1200

    rewrite index

--- client/index.tsx
+++ client/index.tsx
@@ -1,16 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from 'react-dom/client'
-import { router } from '../client/router'
-import { RouterProvider } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
-const root = createRoot(document.getElementById('app') as HTMLElement)
 const queryClient = new QueryClient()
+const root = createRoot(document.getElementById('app') as HTMLElement)
 
 document.addEventListener('DOMContentLoaded', () => {
-  root.render(  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-    <ReactQueryDevtools/>
-  </QueryClientProvider>
+  root.render(
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 })
