18d6e697af1d5f5c760f42b5343c4b1efdc5918a
achoo-o <amyj.etana@gmail.com>
2024-09-26 22:42:07 +1200

    add lighter blue color for contrast

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -2,7 +2,8 @@ export default function About() {
   return (
     <section className="flex flex-col items-center gap-5 mt-44 px-64">
       <h1 className="text-5xl text-white">About</h1>
-      <p className="text-xl text-gray-200">Text here.</p>
+      <p className="text-xl text-gray-200">The background was made using p5.js</p>
+      <a className="text-xl text-blue-100" href="https://editor.p5js.org/achoo-o/sketches/u-pKxOnxV">Sample here.</a>
     </section>
   )
 }
