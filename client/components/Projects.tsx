9f0df926c487e7939b67a8058270185fdff4fe97
achoo-o <amyj.etana@gmail.com>
2024-09-27 10:27:03 +1200

    adjust button height

--- client/components/Projects.tsx
+++ client/components/Projects.tsx
@@ -67,7 +67,7 @@ export default function Projects() {
                   <a href={link} 
                     target="_blank" 
                     rel="noreferrer" 
-                    className="inline-flex items-center mt-3 px-3 py-2 text-sm font-semibold text-center text-white bg-orange-400 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    className="inline-flex items-center mt-3 px-3 py-3 text-sm font-semibold text-center text-white bg-orange-400 rounded-lg  focus:ring-4 focus:outline-none focus:ring-blue-300">
                       View Project
                       <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
