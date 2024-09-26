fb9d3a901b373b5d41cabb30da38886a2cc4423a
achoo-o <amyj.etana@gmail.com>
2024-09-27 11:54:20 +1200

    put sketch into component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet, useMatches } from "react-router-dom";
 import useDocumentTitle from './hooks/use-document-title.ts'
 import Header from "./Header";
+import Art from "./animation/sketch.tsx";
 
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
   useDocumentTitle(pageTitle ? `${pageTitle}` : 'Amy | Developort.')
   return (
     <div>
+      <Art />
       <Header />
       <Outlet />
     </div>
