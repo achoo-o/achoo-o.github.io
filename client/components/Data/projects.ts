64045b044a9421e117ebda4b987c2538f82ddc16
achoo-o <amyj.etana@gmail.com>
2024-10-02 13:14:49 +1300

    separate project data from component, adjust psr text

---
+++ client/components/Data/projects.ts
@@ -0,0 +1,25 @@
+const projects = [
+  {
+    name: 'pawpals', 
+    description: 'Made as part of the DevAcademy course in a group of 5, pawpals is a website made to bring pet-lovers together.', 
+    link: 'https://pawpals-matai-24.devacademy.nz/',
+    thumbnail: 'pawpals.png',
+    alt: ''
+  },
+  {
+    name: 'Whakataukī API', 
+    description: 'This project was made to promote Te Reo Māori usage in the developer space via Whakataukī, Māori "proverbs".', 
+    link: 'https://whakatauki-api.up.railway.app/',
+    thumbnail: 'whakatauki.png',
+    alt: ''
+  },
+  {
+    name: 'My \'First\' Project', 
+    description: 'A simple rock, paper, scissors project made as part of The Odin Project. One of the very first games I deployed.', 
+    link: 'https://achoo-o.github.io/paper-scissors-rock/paperScissorsRock.html',
+    thumbnail: 'rockpaperscissors.png',
+    alt: ''
+    },
+]
+
+export default projects
--- client/components/Pages/Projects.tsx
+++ client/components/Pages/Projects.tsx
@@ -1,31 +1,8 @@
 import { useState } from "react"
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs"
+import projects from "../Data/projects"
 
 export default function Projects() {
-  const projects = [
-    {
-      name: 'pawpals', 
-      description: 'Made as part of the DevAcademy course in a group of 5, pawpals is a website made to bring pet-lovers together.', 
-      link: 'https://pawpals-matai-24.devacademy.nz/',
-      thumbnail: 'pawpals.png',
-      alt: ''
-    },
-    {
-      name: 'Whakataukī API', 
-      description: 'This project was made to promote Te Reo Māori usage in the developer space via Whakataukī, Māori "proverbs".', 
-      link: 'https://whakatauki-api.up.railway.app/',
-      thumbnail: 'whakatauki.png',
-      alt: ''
-    },
-    {
-      name: 'My \'First\' Project', 
-      description: 'A simple rock, paper, scissors project made as part of The Odin Project. One of the very first games I managed to deploy.', 
-      link: 'https://achoo-o.github.io/paper-scissors-rock/paperScissorsRock.html',
-      thumbnail: 'rockpaperscissors.png',
-      alt: ''
-      },
-  ]
-
   const [ slide, setSlide ] = useState(0)
 
   function handleClick(dir: string): void {
